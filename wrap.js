b9fe0aac7404f49072d6befba48e32f7c051a84f
Andy Poon <anpoon430@gmail.com>
2018-08-13 11:32:48 -0400

    wrote tests for wrap function and added wrap function

--- wrap.js
+++ wrap.js
@@ -1,5 +1,31 @@
-const wrap = (str, num) => {
-  if (str.length === 0) return "";
+// const wrap = (str, num) => {
+//   if (str.length === 0) return "";
+//   let i = 0;
+//   let result = "";
+//   while (i < str.length) {
+//     let line = str.slice(i, num + i);
+//     let lastSpace = line.lastIndexOf(" ");
+//     result += str.slice(i, lastSpace) + "\n";
+//     i = lastSpace + 1;
+//   }
+//   return result;
+// };
+
+const wrap = (line, num) => {
+  if (line.length <= num) return line;
+  const indexOfBlank = line.lastIndexOf(" ", num);
+  let split;
+  let offset;
+  if (indexOfBlank > -1) {
+    split = indexOfBlank;
+    offset = 1;
+  } else {
+    split = num;
+    offset = 0;
+  }
+  return (
+    line.substring(0, split) + "\n" + wrap(line.substring(split + offset), num)
+  );
 };
 
 module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -2,7 +2,28 @@ const { expect } = require("chai");
 const wrap = require("./wrap");
 
 describe("wrap", () => {
+  // beforeEach((function(){
+
+  //   wrap(str, num)
+  // }))
   it("Returns empty string if empty string was provided", () => {
     expect(wrap("", 10)).to.equal("");
   });
+  it("it makes sure no line of text is longer than the column number", () => {
+    let text =
+      "Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.";
+    let num = 20;
+    expect(wrap(text, num)).to.equal(
+      `Lorem ipsum dolor\nsit eu amet, elit na\nmagna sem amet nulla\nvel purus ac ligula.`
+    );
+  });
+  it("works with text shorter than column size", () => {
+    let text = "lorem ipsum";
+    let num = 20;
+    expect(wrap(text, num)).to.equal("lorem ipsum");
+  });
+
+  // it('makes sure appropriate data types passed', () => {
+  //   expect()
+  // })
 });
